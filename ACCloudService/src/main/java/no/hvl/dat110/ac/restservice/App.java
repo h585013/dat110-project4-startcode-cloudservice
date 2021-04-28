35b6c8fe43c181d84be29976ec8f12fd4f68b916
h585013 <54100991+h585013@users.noreply.github.com>
2021-04-28 15:53:25 +0200

    xcvbn

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -45,7 +45,15 @@ public class App {
 		
 		// TODO: implement the routes required for the access control service
 		// as per the HTTP/REST operations describined in the project description
+		// må ha noe for GET/accessdevice/log/-returnerer alle access log entries
 		
+		//GET/accessdevice/log/{id} -returnerer en JSON representasjon av entry med id=id
+		
+		//PUT/accessdevice/code skal oppaterer access koden
+		
+		//GET/accessdevice/code skal returnere den nåværede access koden(skal kunne hentes av ardoinoen)
+		
+		//DELETE/accessdevice/log/ skal slette alle entries i access log og returnere en JSON-representasjon av en tom access log i response
     }
     
 }
